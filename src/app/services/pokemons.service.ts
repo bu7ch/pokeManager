d5bd7f100553b8c83209da8a7a21625e04b72418
Rodolphe <dutchr@live.fr>
2021-07-13 11:10:39 +0200

    :sparkles: adding methods on service

--- src/app/services/pokemons.service.ts
+++ src/app/services/pokemons.service.ts
@@ -1,67 +1,14 @@
 import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import { GenerationAPI } from '../models/generation-api';
+import { PokemonAPI } from '../models/pokemon-api';
+import { PokemonsAPI } from '../models/pokemons-api';
 
 @Injectable({
   providedIn: 'root'
 })
 export class PokemonsService {
-  pokemons = [
-    {
-      id: 1,
-      nom: 'Bulbizarre',
-      img: 'https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/1.png',
-      categorie: 'graine',
-      type: ['plante', 'poison'],
-      taille: 0.7,
-      poids: 6.9,
-    },
-    {
-      id: 2,
-      nom: 'Herbizarre',
-      img: 'https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/2.png',
-      categorie: 'graine',
-      type: ['plante', 'poison'],
-      taille: 1,
-      poids: 13,
-    },
-    {
-      id: 3,
-      nom: 'Florizarre',
-      img: 'https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/3.png',
-      categorie: 'graine',
-      type: ['plante', 'poison'],
-      taille: 2,
-      poids: 100,
-    },
-    {
-      id: 4,
-      nom: 'Salamèche',
-      img: 'https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/4.png',
-      categorie: 'lezard',
-      type: ['feu'],
-      taille: 0.6,
-      poids: 0.5,
-    },
-    {
-      id: 5,
-      nom: 'Reptincel',
-      img: 'https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/5.png',
-      categorie: 'flamme',
-      type: ['feu'],
-      taille: 1.1,
-      poids: 19,
-    },
-    {
-      id: 6,
-      nom: 'DracauFeu',
-      img: 'https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/6.png',
-      categorie: 'flamme',
-      type: ['feu', 'vol'],
-      taille: 1.7,
-      poids: 90.5,
-    },
-  ];
+  
   constructor(private http: HttpClient) { }
 
 getGenerations() {
@@ -70,16 +17,20 @@ getGenerations() {
 }
 
   getPokemons() {
-    return this.pokemons
+    let url = 'https://pokeapi.co/api/v2/pokemon/'
+    return this.http.get<PokemonsAPI>(url)
+  }
+  getPokemon(url:any) {
+    return this.http.get<PokemonAPI>(url)
   }
 
-  countPokemons() {
-    return this.pokemons.length;
-  }
+  // countPokemons() {
+  //   return this.pokemons.length;
+  // }
 
-  rechercherPokemon(nomDuPokemon:string){
-    let regex = new RegExp(nomDuPokemon, 'gi');
-    return this.pokemons.filter(pokemon => pokemon.nom.match(regex))
-    console.log()
-  }
+  // rechercherPokemon(nomDuPokemon:string){
+  //   let regex = new RegExp(nomDuPokemon, 'gi');
+  //   return this.pokemons.filter(pokemon => pokemon.nom.match(regex))
+  //   console.log()
+  // }
 }
